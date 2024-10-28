72dd403f3c255a53812fdf5da2e7067b5241aed3
Edwin Nuñez <edwin.nunez@catolica.edu.sv>
2024-10-27 22:06:29 -0600

    Codigo XMAL y CS para la View Edit

--- Views/EditProductoPage.xaml.cs
+++ Views/EditProductoPage.xaml.cs
@@ -1,9 +1,39 @@
-namespace TiendaSmart.Views;
+namespace NubeCRUD.Views;
+using NubeCRUD.Model;
+using NubeCRUD.Conexion;
 
 public partial class EditProductoPage : ContentPage
 {
-	public EditProductoPage()
-	{
-		InitializeComponent();
-	}
+    FirebaseConexion firebaseConexion = new FirebaseConexion();
+    private Producto producto;
+
+    // Constructor que recibe el producto
+    public EditProductoPage(Producto producto)
+    {
+        InitializeComponent();
+        this.producto = producto;
+
+        // Cargar datos del producto en los campos de entrada
+        if (producto != null)
+        {
+            NombreEntry.Text = producto.Nombre;
+            DescripcionEntry.Text = producto.Descripcion;
+            PrecioEntry.Text = producto.Precio.ToString();
+        }
+    }
+
+    private async void OnUpdateProductClicked(object sender, EventArgs e)
+    {
+        // Actualizar los datos del producto
+        producto.Nombre = NombreEntry.Text;
+        producto.Descripcion = DescripcionEntry.Text;
+        producto.Precio = decimal.Parse(PrecioEntry.Text);
+
+        // Enviar los datos actualizados a Firebase
+        await firebaseConexion.UpdateProducto(producto.Id, producto);
+        await DisplayAlert("Éxito", "Producto actualizado", "OK");
+
+        // Regresar a la página anterior
+        await Navigation.PopAsync();
+    }
 }

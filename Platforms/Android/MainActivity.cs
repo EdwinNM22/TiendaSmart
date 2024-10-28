c3a9cc36c85858458ec5bbceda5ed2fa60d66ab5
Edwin Nuñez <edwin.nunez@catolica.edu.sv>
2024-10-27 22:44:06 -0600

    Color de NavBar y StatusBar

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views; 
 
 namespace TiendaSmart
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Cambiar el color de la barra de estado
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#00796B")); // Ajusta el color según tu navbar
+        }
     }
 }

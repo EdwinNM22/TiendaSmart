6c04d32e885c83eca11ee7ab8164ef6c12a2e686
Edwin Nuñez <edwin.nunez@catolica.edu.sv>
2024-10-27 23:03:21 -0600

    Validaciones para la View Edit

--- Views/EditProductoPage.xaml.cs
+++ Views/EditProductoPage.xaml.cs
@@ -1,6 +1,8 @@
 namespace TiendaSmart.Views;
 using TiendaSmart.Model;
 using TiendaSmart.Conexion;
+using System.Text.RegularExpressions;
+
 
 public partial class EditProductoPage : ContentPage
 {
@@ -24,6 +26,29 @@ public partial class EditProductoPage : ContentPage
 
     private async void OnUpdateProductClicked(object sender, EventArgs e)
     {
+        // Verificar si los campos están vacíos
+        if (string.IsNullOrWhiteSpace(NombreEntry.Text) ||
+            string.IsNullOrWhiteSpace(DescripcionEntry.Text) ||
+            string.IsNullOrWhiteSpace(PrecioEntry.Text))
+        {
+            await DisplayAlert("Error", "Por favor, rellena todos los campos antes de actualizar el producto", "Ok");
+            return; // Detener el proceso si algún campo está vacío
+        }
+
+        // Validar que el campo de Nombre solo contenga letras
+        if (!Regex.IsMatch(NombreEntry.Text, @"^[a-zA-Z\s]+$"))
+        {
+            await DisplayAlert("Error", "El campo 'Nombre' solo puede contener letras", "Ok");
+            return;
+        }
+
+        // Verificar si el precio es un número válido
+        if (!decimal.TryParse(PrecioEntry.Text, out decimal precio))
+        {
+            await DisplayAlert("Error", "El campo 'Precio' solo pued contener letras", "Ok");
+            return;
+        }
+
         // Actualizar los datos del producto
         producto.Nombre = NombreEntry.Text;
         producto.Descripcion = DescripcionEntry.Text;

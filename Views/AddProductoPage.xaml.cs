f1dff5575452753b419ee09bfe3cf73cbc1649c1
Edwin Nuñez <edwin.nunez@catolica.edu.sv>
2024-10-27 22:05:16 -0600

    Codigo XMAL y CS para la View de Add

--- Views/AddProductoPage.xaml.cs
+++ Views/AddProductoPage.xaml.cs
@@ -1,9 +1,29 @@
-namespace TiendaSmart.Views;
+using NubeCRUD.Model;
+using NubeCRUD.Conexion;
+namespace NubeCRUD.Views;
 
 public partial class AddProductoPage : ContentPage
 {
-	public AddProductoPage()
-	{
-		InitializeComponent();
-	}
+
+    FirebaseConexion firebaseConexion = new FirebaseConexion();
+
+
+    public AddProductoPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnAddProductClicked(object sender, EventArgs e)
+    {
+        var producto = new Producto()
+        {
+            Nombre = NombreEntry.Text,
+            Descripcion = DescripcionEntry.Text,
+            Precio = decimal.Parse(PrecioEntry.Text),
+        };
+
+        await firebaseConexion.AddProducto(producto);
+        await DisplayAlert("Exito", "Producto agregado", "ok");
+        await Navigation.PopAsync();
+    }
 }
